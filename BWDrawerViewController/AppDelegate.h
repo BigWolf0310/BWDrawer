4ecef9e1219e05f1406c3d2b31b38b430eadc414
Mr_shang <shangyt@shangmaikeji.com>
2019-12-10 16:40:53 +0800

    Initial Commit

---
+++ BWDrawerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWDrawerViewController
+//
+//  Created by syt on 2019/12/10.
+//  Copyright © 2019 syt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
